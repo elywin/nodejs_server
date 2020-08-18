f5cbc869852b41dd725a369c98b14026f8ed47d7
elywin <lukeeraelywin@gmail.com>
2020-08-18 23:18:16 +0300

    ft(add:add listening port)

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ http.createServer((req,res)=>{
     res.writeHead(status,{'content-type':'text/html'});
     res.write('<h1>Hello world!</h1>');
     res.end(); 
+}).listen(port,()=>{
+    console.log(`server listening on http://localhost:${port}`);
 });
 /**
  * Routes Definitions
