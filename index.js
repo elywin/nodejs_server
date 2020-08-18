5504181199d63b8f2b037be87cf2b2e8ea6dd769
elywin <lukeeraelywin@gmail.com>
2020-08-18 23:11:07 +0300

    ft(add:created server)

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+/**
+ * Required External Modules
+ */
+const http = require('http');
+/**
+ * App Variables
+ */
+const port = 2000;
+const status = 200;
+/**
+ *  App Configuration, create server
+ */
+http.createServer((req,res)=>{
+    res.writeHead(status,{'content-type':'text/html'});
+    
+});
+/**
+ * Routes Definitions
+ */
+
+/**
+ * Server Activation
+ */
+
+
+
+
+
